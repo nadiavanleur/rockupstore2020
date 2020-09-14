2552b8547d36125c7937f7a69b30e6d7e270c758
Nadia van Leur <nadiavl@hotmail.com>
2020-09-14 18:26:52 +0200

    added zeit now

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 const clientConfig = {
   url: "http://localhost:3000",
-  graphqlUrl: "https://rockup.store/graphql/",
+  graphqlUrl: "http://rockup.store/graphql/",
 };
 
 export default clientConfig;
