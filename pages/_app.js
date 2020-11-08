3bcc4404566d6565593b9f384f51175110f6ff5c
Nadia van Leur <nadiavl@hotmail.com>
2020-11-08 14:15:56 +0100

    Added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "../assets/scss/styles.scss";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import objectFitImages from "object-fit-images";
+import "../assets/scss/styles.scss";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 objectFitImages();
 
