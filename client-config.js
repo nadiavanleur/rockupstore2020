542a66c8cbe79773cbad77cdc65ef0ae98905d4e
Nadia van Leur <nadiavl@hotmail.com>
2020-12-12 16:53:10 +0100

    - Add cookiebot

--- client-config.js
+++ client-config.js
@@ -2,6 +2,7 @@ const clientConfig = {
   url: "http://localhost:3000",
   liveUrl: "https://www.subflow.nl",
   graphqlUrl: "https://admin.subflow.nl/graphql/",
+  cookiebotCode: "a73f391a-c6ea-4bd5-a9ac-1f4dfcef5a6f",
   address: {
     street: "Ginnekenweg 127A",
     postalCode: "4818JD",
