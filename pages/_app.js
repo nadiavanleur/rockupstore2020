ad6515e271714609e218795e24970b2dd108f129
Nadia van Leur <nadiavl@hotmail.com>
2020-12-29 22:06:12 +0100

    - clear session if session is older than 24 hours

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Router from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import objectFitImages from "object-fit-images";
+import { useEffect } from "react";
 import "../assets/scss/styles.scss";
 
 NProgress.configure({ minimum: 0.8 });
@@ -12,6 +13,18 @@ Router.events.on("routeChangeError", () => NProgress.done());
 objectFitImages();
 
 const App = ({ Component, pageProps }) => {
+  // Clear session if session is older than 24 hours
+  useEffect(() => {
+    const storageCleared = parseInt(
+      localStorage.getItem("storageCleared") || "0"
+    );
+    const yesterday = Date.now() - 1000 * 60 * 60 * 24;
+    if (!storageCleared || storageCleared <= yesterday) {
+      localStorage.clear();
+      localStorage.setItem("storageCleared", Date.now());
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
